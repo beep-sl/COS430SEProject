4575b1c380cf890298ec1df76b36401ddf94d6d1
pmcgorrill <patrickmcgorrill@gmail.com>
2022-03-05 13:55:44 -0500

    middle click to activate swap animation

--- sortVisualizer/main.js
+++ sortVisualizer/main.js
@@ -2,6 +2,7 @@ import './style.css'
 import{ OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
 import {AnimationClip, Clock,  NumberKeyframeTrack, VectorKeyframeTrack , AnimationMixer} from 'three';
 import * as THREE from 'three';
+import * as dat from 'dat.gui';
 
 
 /// set up
@@ -13,10 +14,6 @@ var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeigh
 camera.position.x = 30; 
 camera.position.y = 8;
 camera.position.z = 45;
-
-// camera.rotation.x = 0.004621863619611674
-// camera.rotation.y = 0.11761209161898253
-// camera.rotation.z = -0.0005423385184554795
 
 var renderer = new THREE.WebGL1Renderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -35,13 +32,11 @@ pointlight2.position.set(30,15,-5);
 
 scene.add(pointlight1, pointlight2);
 
-
-
+/// BOXES
 var geometry = new THREE.BoxGeometry(1, 1, 1);
 var material = new THREE.MeshStandardMaterial({color: 0x770070});
 var brightMaterial = new THREE.MeshStandardMaterial({color: 0x1F0FFF});
 
-///0xFF00F0
 var cube = [];
 var length = 23;
 for (let i = 0; i < 23; i++) {
@@ -58,29 +53,18 @@ cube[5].material = brightMaterial;
 
 cube[12].material = brightMaterial;
 
-function animate() {
-  requestAnimationFrame(animate);
-  const delta = clock.getDelta();
-  mixer1.update(delta);
-  mixer2.update(delta);
-  renderer.render(scene, camera);
-
-}
 
 
 ///A basic animation using keyframes
 
-const mixers = swap(cube[5], cube[12]);
+var mixers;
 var mixer1 , mixer2;
-
-mixer1 = mixers[0];
-mixer2 = mixers[1];
 
 
 function swap(cube1, kube2){
   let midway =  (cube1.position.x +  kube2.position.x)/2; 
 
-  let times = [0, 2, 4, 6, 8];
+  let times = [0, 2, 4];
   let cube1_values = [
     cube1.position.x, 
     cube1.position.y, 
@@ -94,13 +78,7 @@ function swap(cube1, kube2){
     cube1.position.y,
     kube2.position.z,
 
-    midway,
-    cube1.position.y, 
-    midway,
 
-    cube1.position.x, 
-    cube1.position.y, 
-    cube1.position.z 
   ];
 
 
@@ -117,13 +95,6 @@ function swap(cube1, kube2){
     kube2.position.y,
     cube1.position.z,
     
-    midway,
-    kube2.position.y, 
-    -midway,
-
-    kube2.position.x, 
-    kube2.position.y, 
-    kube2.position.z 
   ];
 
   let cube1_positionKF = new VectorKeyframeTrack('.position', times, cube1_values);
@@ -136,8 +107,7 @@ function swap(cube1, kube2){
   let tracks2 = [kube2_positionKF];
 
 
-  // use -1 to automatically calculate
-  // the length from the array of tracks
+  // use -1 to automatically calculate the length from the array of tracks
   let len = -1;
 
   let clip1 = new AnimationClip('slowmove_cube', len, tracks1);
@@ -150,7 +120,14 @@ function swap(cube1, kube2){
   let mixer2 = new AnimationMixer(kube2, THREE.InterpolateSmooth);
 
   let action1 = mixer1.clipAction(clip1);
+  action1.setLoop(THREE.LoopOnce);
+  action1.clampWhenFinished =true;
+  action1.enable =true;
+
   let action2 = mixer2.clipAction(clip2);
+  action2.setLoop(THREE.LoopOnce);
+  action2.clampWhenFinished =true;
+  action2.enable =true;
 
   action1.play();
   action2.play();
@@ -159,6 +136,31 @@ function swap(cube1, kube2){
 }
 
 
-///
+/// UI
 
-animate();
+document.body.appendChild( renderer.domElement );
+document.addEventListener( 'auxclick', onPointerDown );
+
+function onPointerDown( event ) {
+  mixers = swap(cube[5], cube[12]);
+  mixer1 = mixers[0];
+  mixer2 = mixers[1];
+  animate();
+}
+
+
+
+//ANIMATION
+function animate() {
+  requestAnimationFrame(animate);
+  const delta = clock.getDelta();
+  mixer1.update(delta);
+  mixer2.update(delta);
+  renderer.render(scene, camera);
+
+}
+
+///
+renderer.render(scene, camera);
+
+// animate();

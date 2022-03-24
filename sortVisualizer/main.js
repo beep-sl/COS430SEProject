677bb5842ae7f853d6bc8ea3335067438b8a4953
pmcgorrill <patrickmcgorrill@gmail.com>
2022-03-24 14:34:42 -0400

    reset Button is working
    very messy needs cleaning, but wanted to share

--- sortVisualizer/main.js
+++ sortVisualizer/main.js
@@ -10,12 +10,12 @@ const clock = new Clock();
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight,
   0.5, 1000);
-
+console.log("Hello World");
 camera.position.x = 30; 
 camera.position.y = 8;
 camera.position.z = 45;
 
-var renderer = new THREE.WebGL1Renderer();
+var renderer = new THREE.WebGL1Renderer({antialias: true});
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
@@ -23,34 +23,39 @@ const controls = new OrbitControls(camera, renderer.domElement);
 
 ///  LIGHTS
 const ambLight = new THREE.AmbientLight(0xFFFFFF, 1);
-scene.add(ambLight);
+
 const pointlight1 = new THREE.PointLight(0x0000FF, 1.5);
 pointlight1.position.set(7,15,5);
 
 const pointlight2 = new THREE.PointLight(0xFF0000, 2);
 pointlight2.position.set(30,15,-5);
 
-scene.add(pointlight1, pointlight2);
+scene.add(ambLight, pointlight1, pointlight2);
 
 /// BOXES
-var geometry = new THREE.BoxGeometry(1, 1, 1);
-var material = new THREE.MeshStandardMaterial({color: 0x770070});
+const basicMaterial = new THREE.MeshStandardMaterial({color: 0x770070});
 var brightMaterial = new THREE.MeshStandardMaterial({color: 0x1F0FFF});
 
 var cube = [];
-var length = 23;
-for (let i = 0; i < 23; i++) {
+const xPositions = [];
+const cubeHeights = [];
+const arrayLength = 23;
+for (let i = 0; i < arrayLength; i++) {
 
   let h = Math.abs(3+Math.random()*10)
+  cubeHeights.push(h);
   let geometry = new THREE.BoxGeometry(1, h, 1);
-  cube[i] = new THREE.Mesh(geometry, material);
-  cube[i].position.x = (i * 2) - (length);
+  cube[i] = new THREE.Mesh(geometry, basicMaterial);
+
+  xPositions.push((i * 2) - (arrayLength));
+  cube[i].position.x = xPositions[i];
   cube[i].position.y += h/2;
   scene.add(cube[i]);
 }
 
-cube[5].material = brightMaterial;
 
+
+cube[5].material = brightMaterial;
 cube[12].material = brightMaterial;
 
 // TODO: COMPARE FUNCTION
@@ -61,10 +66,10 @@ cube[12].material = brightMaterial;
 var mixers;
 var mixer1 , mixer2;
 function swap(cubes, index1, index2){
-
+  
   let cube1 = cubes[index1]; 
   let kube2 = cubes[index2];
-  let midway =  (cube1.position.x +  kube2.position.x)/2; 
+  let midway =  (cube1.position.x + kube2.position.x)/2; 
   let times = [0, 2, 4];
 
   //Positions that the cubes will be at
@@ -106,7 +111,7 @@ function swap(cubes, index1, index2){
   let cube1_mixer = new AnimationMixer(cube1, THREE.InterpolateSmooth);
   let cube1_action = cube1_mixer.clipAction(cube1_clip);
   cube1_action.setLoop(THREE.LoopOnce);
-  cube1_action.clampWhenFinished =true;
+  // cube1_action.clampWhenFinished = true;
   cube1_action.enable =true;
   cube1_action.play();
 
@@ -116,7 +121,7 @@ function swap(cubes, index1, index2){
   let kube2_mixer = new AnimationMixer(kube2, THREE.InterpolateSmooth);
   let kube2_action = kube2_mixer.clipAction(kube2_clip);
   kube2_action.setLoop(THREE.LoopOnce);
-  kube2_action.clampWhenFinished =true;
+  // kube2_action.clampWhenFinished = true;
   kube2_action.enable =true;
   kube2_action.play();
 
@@ -135,27 +140,63 @@ function swap(cubes, index1, index2){
 /// UI
 
 document.body.appendChild( renderer.domElement );
-document.getElementById("swapButton").addEventListener(
-   'click', onButtonClick );
+document.getElementById("swapButton").addEventListener('click', swapOnClick );
+document.getElementById("resetButton").addEventListener('click', resetOnClick );
 
 ///
 
+function reset(){
+
+  for (let i = scene.children.length - 1; i >= 0; i--) {
+    if(scene.children[i].type === "Mesh")
+        scene.remove(scene.children[i]);
+}
+for (let i = 0; i < arrayLength; i++) {
+
+  let geometry = new THREE.BoxGeometry(1, cubeHeights[i], 1);
+  cube[i] = new THREE.Mesh(geometry, basicMaterial);
+
+  cube[i].position.x = xPositions[i];
+  cube[i].position.y += cubeHeights[i]/2;
+  scene.add(cube[i]);
+}
+renderer.render(scene, camera);
+
+}
 
 //ANIMATION
 
-function onButtonClick( event ) {
+// function resetAnimation(mixes){
+//   let delta2 = clock.getDelta();
+//   for(var i = 0; i<mixes.length; i++){
+//     requestAnimationFrame(resetAnimation);
+//     mixes[i].update(delta2);
+//     renderer.render(scene, camera);
+
+//   }
+// }
+
+function resetOnClick(){
+  reset();
+}
+
+function swapOnClick() {
   mixers = swap(cube, 5, 12);
   mixer1 = mixers[0];
   mixer2 = mixers[1];
+
   swapAnimation();
+  mixer1.addEventListener('finished', (/*event*/)=>{mixer1.stopAllAction(); cube[12].position.x = xPositions[12];});
+  mixer2.addEventListener('finished', (/*event*/)=>{mixer2.stopAllAction(); cube[5].position.x = xPositions[5];});
 }
 
 function swapAnimation(){
   requestAnimationFrame(swapAnimation);
-  const delta = clock.getDelta();
+  let delta = clock.getDelta();
   mixer1.update(delta);
   mixer2.update(delta);
   renderer.render(scene, camera);
+
 }
 
 function cameraAnimate() {
